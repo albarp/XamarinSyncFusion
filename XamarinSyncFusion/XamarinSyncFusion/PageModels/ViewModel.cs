d7a73b27e250c5fa1270203fbdc67212cc69d908
Alberto A <alberto.arpini@fmc-ag.com>
2018-03-31 22:04:39 +0200

    Compressed all get started chart commits

---
+++ XamarinSyncFusion/XamarinSyncFusion/PageModels/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace XamarinSyncFusion
+{
+    public class ViewModel
+    {
+        public List<Person> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new List<Person>
+            {
+                new Person{Name = "Alberto", Height=170 },
+                new Person{Name="Pippo", Height=190 },
+                new Person{Name="Minnie", Height=160 },
+                new Person{Name="Pluto", Height=180 }
+            };
+
+        }
+    }
+}
